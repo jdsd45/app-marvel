9c114a8c9cc75a6387010bf3c361fc79e678e8a9
jdossantos <dossantosdamiao.1809@apside-groupe.com>
2021-05-10 11:19:45 +0200

    wip

--- src/app/components/comic-detail/comic-detail.component.ts
+++ src/app/components/comic-detail/comic-detail.component.ts
@@ -5,28 +5,28 @@ import { Comic } from 'src/app/models/comic';
 import { ComicService } from 'src/app/services/comic.service';
 
 @Component({
-  selector: 'app-comic-detail',
-  templateUrl: './comic-detail.component.html',
-  styleUrls: ['./comic-detail.component.scss']
+    selector: 'app-comic-detail',
+    templateUrl: './comic-detail.component.html',
+    styleUrls: ['./comic-detail.component.scss']
 })
 export class ComicDetailComponent implements OnInit {
 
-  comic: Comic
+    comic: Comic
 
-  constructor(
-    private route: ActivatedRoute,
-    private comicService: ComicService,
-  ) { }
+    constructor(
+        private route: ActivatedRoute,
+        private comicService: ComicService,
+    ) { }
 
-  ngOnInit(): void {
-    this.getComic();
-  }
+    ngOnInit(): void {
+        this.getComic();
+    }
 
-  getComic(): void {
-    const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.comicService.findOne(id)
-      .subscribe(comic => this.comic = comic);
+    getComic(): void {
+        const id = Number(this.route.snapshot.paramMap.get('id'));
+        this.comicService.findOne(id)
+            .subscribe(comic => this.comic = comic);
 
-  }
+    }
 
 }
